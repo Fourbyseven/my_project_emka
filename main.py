6c751ddb9e5852a4fb94179af0dd96d13558f90f
Emirlan <emkaumetov@gmail.com>
2024-12-13 16:19:54 +0300

    emka

---
+++ main.py
@@ -0,0 +1,102 @@
+
+# 1
+# name_input = input("Введите ваше имя:       ")
+
+# res = f"Привет:     {name_input}"
+# print(res)
+
+
+# 2
+# num = int(input("Введите первое число:      "))
+# num_2 = int(input("Введите второе число:        "))
+# res = num + num_2
+# print(f"Ваш результат:      {res}")
+
+
+# 3
+
+# num = int(input("Введите число:     "))
+# if num % 2 ==0:
+#     print(f"ваша число четное:      {num}")
+# else:
+#     print(f"Ваша число нечетное:     {num}")
+
+# 4
+
+# list_num = [1, 2, 3, 4, 5]
+
+# res  = list_num[1]
+# res_2 = list_num[4]
+# reult = f"{res}, {res_2}"
+# print(reult)
+
+
+
+
+# 5
+# my_dict =   {
+#     "mouse":    "мышка",
+#     "laptop":   "Ноутбук",
+#     "phone":    "телефон"
+# }
+# print(f"Перевод слов:       ", my_dict['mouse'])
+
+
+# 6
+
+# my_int_list = [2, 4, 5, 6, 1]
+# res = sum(my_int_list)
+# print(res)
+
+
+# 7
+
+# my_input = input("Введите вашу строку:      ")
+# result = my_input[::-1]
+
+# print(f"Ваш обратный отсчет:        {result}")
+
+
+
+# 8
+
+# my_int_1 = int(input("Введите первое число:         "))
+# my_int_2 = int(input("Введите второе число:         "))
+
+# if my_int_1 > my_int_2:
+#     print(f"Этот число {my_int_1} больше чем этот {my_int_2}")
+# elif my_int_2 > my_int_1:
+#     print(f"Этот число {my_int_2} больше чем этот {my_int_1}")
+# else:
+#     print("Обе числа равны:     ")
+
+
+# 9
+
+# my_str_input = input("Введите вашу имя:     ")
+# my_int_input = int(input("Введите ваш возраст:      "))
+
+# res = F"Привет, {my_str_input}, Тебе уже {my_int_input} лет!"
+
+# print(res)
+
+
+# 10
+# numbers = list(range(1, 21))
+
+# result  = [num ** 2 for num  in numbers]
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main_2.py
@@ -0,0 +1,62 @@
+#1
+
+# number = int(input("Введите первое число:       "))
+# number_2 = int(input("Введите второе число:         "))
+# операция = input("Ввдеит на что хотите сложить(+, -, *, /):           ")
+
+# if операция == "+":
+#     num = number + number_2
+
+# elif операция == "-":
+#     num = number - number_2
+
+# elif операция == "*":
+#     num = number * number_2
+    
+# elif операция == "/":
+#     num = number / number_2
+
+# print(f"Ваша число:     {num}")
+
+
+    
+#2
+
+
+# Рассмотреть
+# year = int(input("Введите год: "))
+
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(f"{year} является високосным годом.")
+# else:
+#     print(f"{year} не является високосным годом.")
+
+
+
+
+
+
+
+# 3
+
+# my_num = [3, 21, 52, 72, 12, 76, 31]
+# my_min_num = min(my_num)
+# my_max_num = max(my_num)
+# res = f"Минимальное целое число:        {my_min_num},\n \rМаксимальное число:     {my_max_num}"
+
+# print(res)
+
+
+#4
+
+### Рассмотреть
+# user_input = input("Введите строку: ")
+
+# sorted_string = ''.join(sorted(user_input))
+
+# print("Символы строки в алфавитном порядке:", sorted_string)
+
+
+
+#5
