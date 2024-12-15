1f40e5bb626386dd0e9f46b8c9be469a76e879fd
Emirlan <emkaumetov@gmail.com>
2024-12-15 18:20:12 +0300

    new project_2

--- main_2.py
+++ main_2.py
@@ -123,12 +123,73 @@
 #9
 
 
+# n = int(input("Введите количество чисел Фибоначчи, которые хотите увидеть: "))
 
+# if n <= 0:
+#     print("Пожалуйста, введите положительное число.")
+# else:
 
+#     fibonacci = [0, 1]
+    
 
+#     if n == 1:
+#         fibonacci = [0]
+    
 
+#     for i in range(2, n):
+#         next_number = fibonacci[-1] + fibonacci[-2]
+#         fibonacci.append(next_number)
+    
 
+#     print(f"Первые {n} чисел Фибоначчи: {fibonacci}")
+
+#Фибоначчи
 
 
 
 #10
+
+# translations = {
+#     "cat": "кот",
+#     "dog":  "собака",
+#     "house": "дом",
+# }
+
+# while True:
+#     print("\n Выберите действие:    ")
+#     print("1 - Запросить перевод")
+#     print("2 - Добавить новое слово")
+#     print("3 - Выйти из программы")
+#     choice = input("Ваш выбор  (1/2/3): ")
+
+#     if choice == "1":
+#         word = input("Введите слово для перевода:   ").strip().lower()
+#         translations = translations.get(word)
+#         if translations:
+#             print(f"Перевод слова '{word}':     {translations}")
+#         else:
+#             print(f"Слово '{word}' не найдено в словаре.")
+
+#     elif choice == "2":
+#         new_word = input("Введитек новое слово на английском:   ").strip().lower()
+#         if new_word in translations:
+#             print(f"Слово '{new_word}' уже есть в словаре с переводом:  {translations[new_word]}")
+
+#         else:
+#             new_translation = input("Введите перевод этого слова:   ").strip().lower()
+#             translations[new_word] = new_translation
+#             print(f"Слово '{new_word}' с переводом '{new_translation}' добавлено в словарь")
+
+#     elif choice == "3":
+#         print("Выход из программы. До всидания")
+#         break
+
+#     else:
+#         print("Неверный выбор. Попробуйте снова.")
+
+
+
+
+
+
+
